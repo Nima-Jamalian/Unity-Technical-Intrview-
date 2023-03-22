894666c99f26e8ab5222f2d00a48d3c1d9b6e79f
Nima Jamalian <nima.jamalian80@gmail.com>
2023-03-22 23:53:28 +0000

    Project File

--- Assets/Scripts/FacingDirection.cs
+++ Assets/Scripts/FacingDirection.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class FacingDirection : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
